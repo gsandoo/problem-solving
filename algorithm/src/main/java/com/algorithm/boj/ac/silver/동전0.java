38d3e312d67674583cc35020e0682aeeb2ce644e
sakang <rogersddt@naver.com>
2024-12-25 02:04:59 +0900

    2024-12-25 백준 동전0

--- algorithm/src/main/java/com/algorithm/boj/ac/silver/동전0.java
+++ algorithm/src/main/java/com/algorithm/boj/ac/silver/동전0.java
@@ -1,16 +1,36 @@
 package com.algorithm.boj.ac.silver;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class 동전0 {
 
-    static boolean[] visited;
-    static int n, m, count;
+    static int N, K, answer;
+    static int [] arr;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
+        N= Integer.parseInt(st.nextToken());
+        K= Integer.parseInt(st.nextToken());
+
+        arr = new int[N];
+
+        for (int i = 0 ; i < arr.length; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        for (int i = N-1; i >= 0 ; i--) {
+            if (arr[i] <= K){
+               answer += (K/arr[i]);
+               K = K%arr[i];
+            }
+        }
+
+        System.out.println(answer);
     }
 }
