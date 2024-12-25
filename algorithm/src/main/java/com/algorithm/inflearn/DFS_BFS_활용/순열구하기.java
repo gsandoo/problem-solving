4d1cf8feb2d013d2ed7013e7ea6992883121564a
sakang <rogersddt@naver.com>
2024-12-25 13:15:15 +0900

    2024-12-25 순열 구하기

---
+++ algorithm/src/main/java/com/algorithm/inflearn/DFS_BFS_활용/순열구하기.java
@@ -0,0 +1,51 @@
+package com.algorithm.inflearn.DFS_BFS_활용;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 순열구하기 {
+
+    static int  n, m;
+    static int[] arr, visited, pm;
+
+    static StringTokenizer st;
+
+    public static void dfs(int level, int[] arr){
+        if(level == m) { // 2
+            for (int x: pm)System.out.print(x+" ");
+            System.out.println();
+        }else{
+            for (int i = 0; i< arr.length;i++){
+                if(visited[i] == 0) {
+                    pm[level] = arr[i]; // 3
+                    visited[i] = 1; // 1 0 0
+                    dfs(level+1, arr); //
+                    visited[i] = 0;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args)throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[n];
+        visited = new int[n];
+        pm = new int[m];
+
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0 ; i < arr.length ; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        dfs(0,arr);
+    }
+}
