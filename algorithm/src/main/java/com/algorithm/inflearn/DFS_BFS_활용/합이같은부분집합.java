86423de63c50575a6296742785bbd4d3e187fd06
sakang <rogersddt@naver.com>
2025-01-17 01:11:18 +0900

    2025-1-15 dfs 복기

--- algorithm/src/main/java/com/algorithm/inflearn/DFS_BFS_활용/합이같은부분집합.java
+++ algorithm/src/main/java/com/algorithm/inflearn/DFS_BFS_활용/합이같은부분집합.java
@@ -3,34 +3,32 @@ package com.algorithm.inflearn.DFS_BFS_활용;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class 합이같은부분집합 {
 
+    static int N, total;
     static String answer = "NO";
-    static boolean flag = false;
-    static int N, total = 0;
 
-    public static void dfs(int L, int sum, int[]arr) {
-        if (flag) return;
-        if (L == N) {
-            if (total/2 == sum) {
-                flag = true;
-                answer = "YES";
-            }
-        }
-        else{
-            dfs(L+1, sum+arr[L], arr);
-            dfs(L+1, sum, arr);
-        }
+    static boolean flag = false;
+    private static void dfs(int L, int sum,int[] arr){
+       if (flag) return;
+       if (L == N){
+           if (total / 2 == sum) answer = "YES";
+           flag = true;
+       }else{
+           dfs(L+1, sum+arr[L], arr);
+           dfs(L+1, sum, arr);
+       }
     }
 
     public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        String [] nStr = br.readLine().split(" ");
-        int[] arr = new int[nStr.length];
-        for (int i = 0 ; i < nStr.length; i++) {
-            arr[i] = Integer.parseInt(nStr[i]);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[N];
+        for (int i = 0 ; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
             total += arr[i];
         }
         dfs(0,0, arr);
