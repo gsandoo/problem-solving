84c851faeefb08d2299c9206ddc3fbd2c03c6088
sakang <rogersddt@naver.com>
2025-01-17 23:26:13 +0900

    2025-1-17 N과M 4

--- algorithm/src/main/java/com/algorithm/boj/solvedac/silver/N과M4.java
+++ algorithm/src/main/java/com/algorithm/boj/solvedac/silver/N과M4.java
@@ -5,34 +5,32 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-// tier: silver 3
-// link: https://www.acmicpc.net/problem/15650
-public class N과M2 {
+public class N과M4 {
 
-    static int [] visited, made;
     static int N, M;
-
-
+    static int [] made;
     public static void dfs(int L, int at){
         if (L == M){
-            for (int m: made) System.out.print(m + " ");
+            for (int m : made) System.out.print(m + " ");
             System.out.println();
         }else{
-           for (int i = at ; i <= N; i++){
-               made[L] = i;
-               dfs(L+1, i+1);
-           }
+            for (int i = at ; i <= N; i++){
+                made[L] = i;
+                dfs(L+1, i);
+            }
         }
     }
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
         made = new int[M];
+
         dfs(0, 1);
+
     }
 }
