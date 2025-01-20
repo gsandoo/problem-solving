ac333450ddcc3a4b7777f71cd1896b3ba8e7f1d0
sakang <rogersddt@naver.com>
2025-01-20 17:43:28 +0900

    2025-1-20 백준

---
+++ algorithm/src/main/java/com/algorithm/programmers/멀쩡한사각형/Solution.java
@@ -0,0 +1,11 @@
+package com.algorithm.programmers.멀쩡한사각형;
+
+import java.math.BigInteger;
+
+public class Solution {
+
+    public long solution(int w, int h) {
+        int gcd = BigInteger.valueOf(w).gcd(BigInteger.valueOf(h)).intValue();
+        return ((long) w * (long) h) - ((((long) w / gcd) + ((long) h / gcd) - 1) * gcd);
+    }
+}
