6e962c41c2dad4a45edf28d18cee90a0ae761fb8
sakang <rogersddt@naver.com>
2025-01-23 11:32:27 +0900

    2025-1-23 solved ac 부녀회장이 될테야

--- algorithm/src/main/java/com/algorithm/boj/bronze/부녀회장이될테야.java
+++
@@ -1,4 +0,0 @@
-package com.algorithm.boj.bronze;
-
-public class 부녀회장이될테야 {
-}
---
+++ algorithm/src/main/java/com/algorithm/boj/solvedac/bronze/부녀회장이될테야.java
@@ -0,0 +1,36 @@
+package com.algorithm.boj.solvedac.bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class 부녀회장이될테야 {
+
+    static int[][] apartment;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        apartment = new int[15][15];
+        for (int i = 1; i < 15; i++) {
+            apartment[i][1] = 1 ;
+            apartment[0][i] = i ; // 기본 값 초기화
+        }
+
+        for (int i = 1; i <= 14 ; i++ ){
+            for (int j = 1; j <= 14; j++){
+                apartment[i][j] = apartment[i][j-1] + apartment[i-1][j];
+            }
+        }
+
+        for (int i = 0; i < T; i++) {
+            int k = Integer.parseInt(br.readLine());
+            int n = Integer.parseInt(br.readLine());
+
+            System.out.println(apartment[k][n]);
+        }
+
+    }
+}
