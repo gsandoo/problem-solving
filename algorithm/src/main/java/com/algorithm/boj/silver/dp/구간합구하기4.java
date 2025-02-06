ae53c723664a8f1d27967702dbfebda4ed0468f7
sakang <rogersddt@naver.com>
2025-02-06 11:57:49 +0900

    2025-2-6 구간 합 구하기

---
+++ algorithm/src/main/java/com/algorithm/boj/silver/dp/구간합구하기4.java
@@ -0,0 +1,40 @@
+package com.algorithm.boj.silver.dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import java.util.StringTokenizer;
+
+// https://www.acmicpc.net/problem/11659
+public class 구간합구하기4 {
+
+    static int N, M, i, j;
+    static int[] arr, dp;
+    static StringTokenizer st;
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[N+1];
+        dp = new int[N+1];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i  <= N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            dp[i] = dp[i-1] + arr[i];
+        }
+
+        for (int x = 0 ; x < M; x++){
+            st = new StringTokenizer(br.readLine());
+            i = Integer.parseInt(st.nextToken());
+            j = Integer.parseInt(st.nextToken());
+            System.out.println(dp[j]-dp[i-1]);
+        }
+    }
+
+}
