fdb328223ea3518e4f27f73e49ebf39dea751a2c
sakang <rogersddt@naver.com>
2025-05-08 16:52:17 +0900

    2025-05-08 프로그래머스 고득점 kit 완주 하지 못한 선수

---
+++ algorithm/src/main/java/com/algorithm/programmers/고득점kit/LV0/완주하지못한선수.java
@@ -0,0 +1,23 @@
+package com.algorithm.programmers.고득점kit.LV0;
+
+import java.util.*;
+
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        HashMap<String, Integer> hm = new HashMap<>();
+        for(String p : participant){
+            hm.put(p, hm.getOrDefault(p, 0)+1);
+        }
+        for (String c : completion){
+            hm.put(c, hm.getOrDefault(c, 0)-1);
+        }
+
+        for (Map.Entry<String, Integer> map : hm.entrySet()) {
+            if (map.getValue() != 0) answer = map.getKey();
+        }
+        return answer;
+    }
+}
+
