5ef1628596e6ed886bb014e85c44f2b75d41f513
sakang <rogersddt@naver.com>
2025-05-31 23:42:51 +0900

    2025-05-31 백준 기차가어둠을헤치고은하수를

---
+++ algorithm/src/main/java/com/algorithm/boj/silver/구현/기차가어둠을헤치고은하수를.java
@@ -0,0 +1,44 @@
+package com.algorithm.boj.silver.구현;
+
+import java.io.*;
+import java.util.*;
+
+class 기차가어둠을헤치고은하수를 {
+    static StringTokenizer st;
+    static int N,M;
+    static int[] train;
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        train = new int [N];
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int cmd = Integer.parseInt(st.nextToken());
+            int trainNum = Integer.parseInt(st.nextToken()) - 1;
+
+            if (cmd == 1) {
+                int seat = Integer.parseInt(st.nextToken()) - 1;
+                train[trainNum] |= (1 << seat);
+            } else if (cmd == 2) {
+                int seat = Integer.parseInt(st.nextToken()) - 1;
+                train[trainNum] &= ~(1 << seat);
+            } else if (cmd == 3) {
+                train[trainNum] <<= 1;
+                train[trainNum] &= ~(1 << 20);
+            } else if (cmd == 4) {
+                train[trainNum] >>= 1;
+            }
+        }
+
+        Set<Integer> set = new HashSet<>();
+        for (int t : train) {
+            set.add(t);
+        }
+        System.out.println(set.size());
+    }
+}
