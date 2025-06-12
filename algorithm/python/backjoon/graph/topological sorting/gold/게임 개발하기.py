1fce77245bcfbfb20caf460afcff2d22cc0bf31b
sakang <rogersddt@naver.com>
2025-06-13 00:06:03 +0900

    2025-06-13

---
+++ algorithm/python/backjoon/graph/topological sorting/gold/게임 개발하기.py
@@ -0,0 +1,37 @@
+from collections import deque
+
+n = int(input())
+a = [[] for _ in range (n + 1)]
+indegree = [0] * (n+1)
+selfbuild = [0] * (n+1)
+
+for i in range(1, n+1):
+    inputList = list(map(int, input().split()))
+    selfbuild[i] = (inputList[0])
+    idx = 1
+    while True:
+        preTmp = inputList[idx]
+        idx +=1
+        if preTmp == -1:
+            break
+        a[preTmp].append(i)
+        indegree[i] += 1
+
+queue = deque()
+
+for i in range(1, n+1):
+    if indegree[i] == 0:
+        queue.append(i)
+
+result = [0] * (n+1)
+
+while queue:
+    now = queue.popleft()
+    for next in a[now]:
+        indegree[next] -= 1
+        result[next] = max(result[next], result[now]+ selfbuild[now])
+        if indegree[next] == 0:
+            queue.append(next)
+
+for i in range(1 , n+1):
+    print(result[i] + selfbuild[i])
