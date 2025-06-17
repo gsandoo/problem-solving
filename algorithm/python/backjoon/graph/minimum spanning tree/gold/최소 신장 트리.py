299325f5d336088cba3f111d3919fa46d45fc78e
sakang <rogersddt@naver.com>
2025-06-17 10:37:04 +0900

    2025-06-17

--- algorithm/python/backjoon/graph/minimum spanning tree/gold/최소 신장 트리.py
+++ algorithm/python/backjoon/graph/minimum spanning tree/gold/최소 신장 트리.py
@@ -2,6 +2,7 @@
 
 import sys
 from queue import PriorityQueue
+sys.setrecursionlimit(10**6)
 
 input = sys.stdin.readline
 pq= PriorityQueue()
@@ -10,7 +11,10 @@ n,m = map(int, input().split())
 
 parent = [0] * (n+1)
 
-for i in range(e):
+for i in range(n+1):
+    parent[i] = i 
+
+for i in range(m):
     s,e,w = map(int, input().split())
     pq.put((w,s,e)) #제일 앞 순서로 정렬 되므로 가중치를 제일 앞으로 함
 
@@ -31,6 +35,7 @@ useEdge = 0
 result = 0
 
 while useEdge < n-1 : # mst는 항상 n-1개의 에지를 사용함 -> 그래프 이론
+    w, s, e = pq.get()
     if find(s) != find(e): # 같은 부모가 아닌 경우에만 연결
         union(s,e)
         result += w
