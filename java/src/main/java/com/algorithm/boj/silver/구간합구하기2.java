95b300420b493cd46cb39b4ff6a2c2272c97bb6b
sakang <rogersddt@naver.com>
2025-06-23 16:19:44 +0900

    2025-06-23 구간 합 구하기2 백준

--- java/src/main/java/com/algorithm/boj/silver/구간합구하기2.java
+++ java/src/main/java/com/algorithm/boj/silver/구간합구하기2.java
@@ -7,36 +7,28 @@ import java.util.StringTokenizer;
 
 public class 구간합구하기2 {
     static StringTokenizer st;
-    public static void main(String[] args)throws IOException{
+
+    public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         st = new StringTokenizer(br.readLine());
+
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
+        int[][] arr = new int[n+1][n+1];
+        int[][] sumArr = new int[n+1][n+1];
 
-        int[][]arr = new int[n][n];
 
-        for (int i = 0 ; i < n; i ++) {
+        for (int i = 1; i <= n; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0 ; j < n; j ++) {
+            for (int j = 1; j <= n; j++) {
                 arr[i][j] = Integer.parseInt(st.nextToken());
+                // 누적 합 계산
+                sumArr[i][j] = sumArr[i-1][j] + sumArr[i][j-1] - sumArr[i-1][j-1] + arr[i][j];
             }
         }
 
-        int [][] sumArr = new int[n][n];
-        sumArr[0][0] = arr[0][0];
-
-        for (int i = 1; i < n; i++){
-            sumArr[0][i] = sumArr[0][i-1] + arr[0][i];
-            sumArr[i][0] = sumArr[i-1][0] + arr[i][0];
-        }
-
-        for(int i = 1 ; i < n ; i ++) {
-            for (int j = 1; j < n; j++){
-                sumArr[i][j] = sumArr[i][j-1] + sumArr[i-1][j] - sumArr[i-1][j-1] + arr[i][j];
-            }
-        }
-
-        for (int i = 0 ; i < m ; i++){
+        // 질의 처리
+        for (int q = 0; q < m; q++) {
             st = new StringTokenizer(br.readLine());
             int x1 = Integer.parseInt(st.nextToken());
             int y1 = Integer.parseInt(st.nextToken());
