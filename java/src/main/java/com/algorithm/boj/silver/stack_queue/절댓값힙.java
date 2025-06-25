c168923423b15517b435c9f1eb203dddac3f97ab
sakang <rogersddt@naver.com>
2025-06-25 15:25:28 +0900

    2025-06-25 절댓값 힙

---
+++ java/src/main/java/com/algorithm/boj/silver/stack_queue/절댓값힙.java
@@ -0,0 +1,34 @@
+package com.algorithm.boj.silver.stack_queue;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+
+public class 절댓값힙 {
+
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pq = new PriorityQueue<>(
+                ((o1, o2) -> {
+                    int ma = Math.abs(o1);
+                    int mb = Math.abs(o2);
+                    if (ma == mb) return Integer.compare(ma,mb);
+                    return Integer.compare(ma,mb);
+                }));
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0 ; i < n ; i++) {
+            int tmp = Integer.parseInt(br.readLine());
+            if (tmp == 0) {
+                sb.append(pq.isEmpty() ? 0 : pq.poll()).append("\n");
+            } else {
+                pq.offer(tmp);
+            }
+        }
+
+        System.out.print(sb);
+    }
+}
