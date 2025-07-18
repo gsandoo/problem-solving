17d40a1db5acc0d0105474a4c551239c18d11bcc
sakang <rogersddt@naver.com>
2025-07-18 10:07:26 +0900

    2025-07-18

---
+++ python/programmers/LV.2/더 맵게.py
@@ -0,0 +1,18 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/42626
+
+import heapq
+def solution(scoville, K):
+    answer = 0
+    heapq.heapify(scoville)
+    
+    while scoville[0] < K and len(scoville) >= 2:
+        first = heapq.heappop(scoville)
+        second = heapq.heappop(scoville)
+        nxt = first + (second * 2)
+        heapq.heappush(scoville, nxt)
+        answer += 1
+    return answer if scoville[0] >= K else -1
+
+scoville = [1, 2, 3, 9, 10, 12]	
+K = 7
+print(solution(scoville,K))
