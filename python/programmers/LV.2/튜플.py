3b693652163f44521a60fb6571592811b3fe0af6
sakang <rogersddt@naver.com>
2025-07-26 15:41:14 +0900

    주석 제거

--- python/programmers/LV.2/튜플.py
+++ python/programmers/LV.2/튜플.py
@@ -8,8 +8,6 @@ def solution(s):
     
     arr = list(list(map(int, pt.split(',')))for pt in p)
     
-    
-    # 각 s별 크기 파악 
     arr.sort(key = len)
     
     st = set()
@@ -18,10 +16,5 @@ def solution(s):
             if i not in st:
                 st.add(i)
                 answer.append(i)
-            
 
-    # 리턴
     return answer
-
-s  = "{{2},{2,1},{2,1,3},{2,1,3,4}}"	
-print(solution(s))
