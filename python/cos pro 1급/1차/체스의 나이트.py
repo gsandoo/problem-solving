d509fc04905b223b7cf7c8e3922d9b014ce70fa9
sakang <rogersddt@naver.com>
2025-09-08 18:13:01 +0900

    2025-09-08

--- python/cos pro 1급/1차/체스의 나이트.py
+++ python/cos pro 1급/1차/체스의 나이트.py
@@ -1,7 +1,6 @@
 def solution(pos):
 	answer = 0
 	dx, dy = [1,2,2,1,-1,-2,-2,-1] , [-2,-1,1,2,2,1,-1,-2] # 체스 말 이동경로
-	a = [[0]*8 for _ in range(8)]
 	x,y = ord(str(pos[0]))-65, 8-int(pos[1])
 	for i in range(8):
 		nx, ny = x + dx[i], y + dy[i] # [1,-1],[0,2],[2,2],[3,1],[3,-1],[2,-2],[0,-2],[-1,-1]
